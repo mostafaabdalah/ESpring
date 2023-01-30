22190b103ed3f319540c78687ff5290d33fee532
Mostafa abdallah <34196002+mostafaabdalah@users.noreply.github.com>
2023-01-30 14:52:04 +0200

    @Qualifier

--- src/com/spring/ann/Master.java
+++ src/com/spring/ann/Master.java
@@ -1,16 +1,14 @@
 package com.spring.ann;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component("master")
 public class Master implements MasterInterface{
 
 	@Autowired
-	public Master(CompanyInterface ci) {
-		this.ci = ci;
-	}
-
+	@Qualifier("user")
 	private CompanyInterface ci;
 
 	@Override
